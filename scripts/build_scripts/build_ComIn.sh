00fc5a4c33e4cbfb7a125648e3603e88d75ed8e8
Quan <quan.liu@mpimet.mpg.de>
2025-01-26 17:51:09 +0100

    path

--- scripts/build_scripts/build_ComIn.sh
+++ scripts/build_scripts/build_ComIn.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+export ICONDIR=../../ICON/icon-model  # relative path to ICON source code
+export ICON_BUILD_DIR=../../build/gcc  # relative path to ICON build directory
 
 source ../../environment/spack_initialize.sh
-cd $ICONDIR/build
+cd $ICON_BUILD_DIR
 
 spack load py-mpi4py
 cd externals/comin/build && cmake -DCOMIN_ENABLE_EXAMPLES=ON  -DCOMIN_ENABLE_PYTHON_ADAPTER=ON .
 
-cd $ICONDIR/build/externals/comin/build && make
+cd $ICON_BUILD_DIR/externals/comin/build && make
