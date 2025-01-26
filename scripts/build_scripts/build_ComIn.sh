6bf5d28d84de7ad2c1fd70c097c813a7cbdc0b5a
Quan <quan.liu@mpimet.mpg.de>
2025-01-26 17:56:33 +0100

    issue

--- scripts/build_scripts/build_ComIn.sh
+++ scripts/build_scripts/build_ComIn.sh
@@ -5,7 +5,7 @@ export ICON_BUILD_DIR=../../build/gcc  # relative path to ICON build directory
 source ../../environment/spack_initialize.sh
 cd $ICON_BUILD_DIR
 
-spack load py-mpi4py
+spack load py-mpi4py  # issue: should we load the python environment here?
 cd externals/comin/build && cmake -DCOMIN_ENABLE_EXAMPLES=ON  -DCOMIN_ENABLE_PYTHON_ADAPTER=ON .
 
 cd $ICON_BUILD_DIR/externals/comin/build && make
