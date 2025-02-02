0cbb5963605aa81f82bcd1a91d49d50b8c025ebf
Quan <quan.liu@mpimet.mpg.de>
2025-02-02 15:46:52 +0100

    ICONDIR is not needed

--- scripts/build_scripts/build_ComIn.sh
+++ scripts/build_scripts/build_ComIn.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export ICONDIR=../../ICON/icon-model  # relative path to ICON source code
 export ICON_BUILD_DIR=../../build/gcc  # relative path to ICON build directory
 
 source ../../environment/spack_initialize.sh
