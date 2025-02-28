05cd15bcd1064f68c1f7e47f3e6cc5d2a49b29db
Quan <quan.liu@mpimet.mpg.de>
2025-02-28 17:28:51 +0100

    update path

--- scripts/build_scripts/python_env.sh
+++ scripts/build_scripts/python_env.sh
@@ -4,5 +4,5 @@ BASE_DIR="$(git rev-parse --show-toplevel)" # where the repository is located
 
 spack load /fwv # this loads a proper python installation
 python -m venv $BASE_DIR/environment/python/py_venv
-../../environment/python/py_venv/bin/activate
+$BASE_DIR/environment/python/py_venv/bin/activate
 pip install pandas
