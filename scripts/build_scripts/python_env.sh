2637042834467f89efa43f10ffad6bb9112569ab
Quan <quan.liu@mpimet.mpg.de>
2026-01-26 11:28:54 +0100

    add mpi4py

--- scripts/build_scripts/python_env.sh
+++ scripts/build_scripts/python_env.sh
@@ -3,8 +3,10 @@ module load git
 BASE_DIR="$(git rev-parse --show-toplevel)" # where the repository is located
 
 spack load /fwv # this loads a proper python installation
+spack load /mnm # tells mpi4py which MPI to use
+
 python -m venv $BASE_DIR/environment/python/py_venv
 
 # activate the environment
 source $BASE_DIR/environment/python/py_venv/bin/activate
-pip install pandas
+pip install pandas numpy mpi4py xarray
