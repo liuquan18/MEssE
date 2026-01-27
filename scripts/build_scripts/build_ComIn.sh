4dc53df323b71c0f7c752cc87db415794ef7ec7d
Quan <quan.liu@mpimet.mpg.de>
2026-01-27 11:08:52 +0100

    adapt to new path

--- scripts/build_scripts/build_ComIn.sh
+++ scripts/build_scripts/build_ComIn.sh
@@ -2,12 +2,14 @@
 module load git
 
 BASE_DIR="$(git rev-parse --show-toplevel)" # where the repository is located
-export ICON_BUILD_DIR=$BASE_DIR/build/gcc  # relative path to ICON build directory
-
-source $BASE_DIR/environment/spack_initialize.sh
-cd $ICON_BUILD_DIR
+export ICON_BUILD_DIR=$BASE_DIR/build/messe_env/build_dir/icon-model  # relative path to ICON build directory
 
 spack load py-mpi4py  # issue: should we load the python environment here?
+
+source $BASE_DIR/build/messe_env/py_env/bin/activate
+cd $ICON_BUILD_DIR
+
+
 cd externals/comin/build && cmake -DCOMIN_ENABLE_EXAMPLES=ON  -DCOMIN_ENABLE_PYTHON_ADAPTER=ON .
 
 cd $ICON_BUILD_DIR/externals/comin/build && make
