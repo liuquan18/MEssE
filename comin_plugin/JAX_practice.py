2012eb8a03291d7c328340d65f485730855615ea
Quan <quan.liu@mpimet.mpg.de>
2026-03-19 16:08:31 +0100

    start a practice

---
+++ comin_plugin/JAX_practice.py
@@ -0,0 +1,20 @@
+# %%
+import comin
+
+#%%
+import sys
+import os
+
+import numpy as np
+import json
+import glob
+import math
+from datetime import datetime
+
+import mpi4py.MPI as MPI
+# %%
+import jax
+import jax.numpy as jnp
+print("Using jax", jax.__version__)
+
+# %%
